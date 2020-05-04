dc69a13fb7a6bd90bb789fc19e19bc1e662cdd41
simon <simonzhang0428@gmail.com>
2020-05-03 19:59:00 -0700

    Update recursion1.java

--- recursion1.java
+++ recursion1.java
@@ -43,3 +43,30 @@ public int countX(String str) {
   }
   return countX(str.substring(1));
 }
+
+
+// The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+//
+//
+// fibonacci(0) → 0
+// fibonacci(1) → 1
+// fibonacci(2) → 1
+
+public int fibonacci(int n) {
+  if(n == 0) return 0;
+  if(n == 1) return 1;
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
+// Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+//
+//
+// powerN(3, 1) → 3
+// powerN(3, 2) → 9
+// powerN(3, 3) → 27
+
+public int powerN(int base, int n) {
+  if(base == 1) return 1;
+  if(n == 1) return base;
+  return base * powerN(base, n-1);
+}
